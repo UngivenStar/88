a17cf96847f8ffedb1ec2a1bfc9be311fbf84403
Ungivenstar <jackynight201@gmail.com>
2025-06-24 19:12:55 -0600

    prueba2

--- pou/mvimiento_Player.gd
+++ pou/mvimiento_Player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const MAX_JUMPS = 2
 
-@onready var animationPlayer= $AnimationPlayer
-@onready var sprite2D=$Sprite2D
+var jumps = 0
+
+@onready var animationPlayer = $AnimationPlayer
+@onready var sprite = $Sprite2D
 
 
 
@@ -13,16 +15,19 @@ func _ready():
 	add_to_group("personaje")
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Agregar gravedad si no estamos en el suelo
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		# Reiniciar los saltos al tocar el suelo
+		jumps = 0
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# Manejo de salto (hasta dos saltos)
+	if Input.is_action_just_pressed("ui_accept") and jumps < MAX_JUMPS:
 		velocity.y = JUMP_VELOCITY
+		jumps += 1
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Movimiento horizontal
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -30,14 +35,17 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-	
+
+	# Animaciones
 	animations(direction)
-	
-	if direction ==1:
-		sprite2D.flip_h = false
+
+	# Flip del sprite
+	if direction == 1:
+		sprite.flip_h = false
 	elif direction == -1:
-		sprite2D.flip_h = true
-		
+		sprite.flip_h = true
+
+
 func animations(direction):
 	if is_on_floor():
 		if direction == 0:
@@ -49,6 +57,13 @@ func animations(direction):
 			animationPlayer.play("jump")
 		elif velocity.y > 0:
 			animationPlayer.play("fall")
+
+
+
+
+
+func _on_reser_area_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
 
 func _on_reset_area_body_entered(body: Node2D) -> void:
 	get_tree().reload_current_scene()
